dcb112c15c198d73150b4f120958afbb6b2daedb
unknown <janislacis06@gmail.com>
2023-09-30 21:05:20 +0300

    Finish landing page (except shadow)

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -19,8 +19,28 @@ const LandingPage = () => {
     return (
         <section className="landing-page">
             <div className="starter-page">
-                <h1 className="title">EasyTrading</h1>
-                <h6>Trading screeners, trackers and more</h6>
+                <div>
+                    <h1 className="title">EasyTrading</h1>
+                    <h5>Trading screeners, trackers and more</h5>
+                </div>
+                <button type="button">
+                    <div>
+                        <h6>Get Started</h6>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            className="w-6 h-6">
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                            />
+                        </svg>
+                    </div>
+                </button>
             </div>
             <img
                 className="background-image"
@@ -28,6 +48,7 @@ const LandingPage = () => {
                 alt="background image"
             />
             <div className="decorative-landing-div"></div>
+            <div className="landing-shadow"></div>
         </section>
     )
 }
