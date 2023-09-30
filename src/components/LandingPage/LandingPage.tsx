47dad2ab2d1e6c12d419122057e49013ab8fe89b
Janis <janislacis06@gmail.com>
2023-09-30 19:02:12 +0300

    SCC

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,4 +1,3 @@
-import {Link} from "react-router-dom"
 import backImage from "../../photos/candlestick-chart.jpg"
 import "./landingpage.css"
 import {useEffect} from "react"
@@ -19,15 +18,16 @@ const LandingPage = () => {
 
     return (
         <section className="landing-page">
+            <div className="starter-page">
+                <h1 className="title">EasyTrading</h1>
+                <h6>Trading screeners, trackers and more</h6>
+            </div>
             <img
                 className="background-image"
                 src={backImage}
                 alt="background image"
             />
             <div className="decorative-landing-div"></div>
-            <div className="starter-page">
-                <h1 className="title">EasyTrading</h1>
-            </div>
         </section>
     )
 }
