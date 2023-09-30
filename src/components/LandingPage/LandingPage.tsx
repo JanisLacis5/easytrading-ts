6e5303c2dc883958fcf530971ddd3a9eb319043e
unknown <janislacis06@gmail.com>
2023-09-30 23:11:13 +0300

    Copletely finish hero sectiuon

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -18,37 +18,39 @@ const LandingPage = () => {
 
     return (
         <section className="landing-page">
-            <div className="starter-page">
-                <div>
-                    <h1 className="title">EasyTrading</h1>
-                    <h5>Trading screeners, trackers and more</h5>
-                </div>
-                <button type="button">
+            <section className="hero">
+                <div className="starter-page">
                     <div>
-                        <h6>Get Started</h6>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="1.5"
-                            stroke="currentColor"
-                            className="w-6 h-6">
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M8.25 4.5l7.5 7.5-7.5 7.5"
-                            />
-                        </svg>
+                        <h1 className="title">EasyTrading</h1>
+                        <h5>Trading screeners, trackers and more</h5>
                     </div>
-                </button>
-            </div>
-            <img
-                className="background-image"
-                src={backImage}
-                alt="background image"
-            />
-            <div className="decorative-landing-div"></div>
-            <div className="landing-shadow"></div>
+                    <button type="button">
+                        <div>
+                            <h6>Get Started</h6>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                className="w-6 h-6">
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                                />
+                            </svg>
+                        </div>
+                    </button>
+                </div>
+                <img
+                    className="background-image"
+                    src={backImage}
+                    alt="background image"
+                />
+                <div className="landing-shadow"></div>
+            </section>
+            <section className="about" id="about"></section>
         </section>
     )
 }
