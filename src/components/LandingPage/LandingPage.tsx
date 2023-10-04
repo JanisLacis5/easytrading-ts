4bd2e9b247d36e897c2c901c6094c875f62c6844
unknown <janislacis06@gmail.com>
2023-10-04 22:56:42 +0300

    Add login button on hero

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -29,24 +29,44 @@ const LandingPage = () => {
                         <h1 className="title">EasyTrading</h1>
                         <h5>Trading screeners, trackers and more</h5>
                     </div>
-                    <Link to="/login">
-                        <div>
-                            <h6>Get Started</h6>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1.5}
-                                stroke="currentColor"
-                                className="w-6 h-6">
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M8.25 4.5l7.5 7.5-7.5 7.5"
-                                />
-                            </svg>
-                        </div>
-                    </Link>
+                    <div className="landing-buttons">
+                        <Link to="/login">
+                            <div>
+                                <h6>Login</h6>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="w-6 h-6">
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+                                    />
+                                </svg>
+                            </div>
+                        </Link>
+                        <Link to="/signup">
+                            <div>
+                                <h6>Get Started</h6>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="w-6 h-6">
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                                    />
+                                </svg>
+                            </div>
+                        </Link>
+                    </div>
                 </div>
                 <img
                     className="background-image"
