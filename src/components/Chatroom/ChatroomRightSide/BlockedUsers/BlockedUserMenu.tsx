f97bc034aa8fab39442d5f2ea2bb0749b3c54c53
Janis <janislacis06@gmail.com>
2023-11-17 14:05:52 +0200

    Make UNBLOCK USER function

--- src/components/Chatroom/ChatroomRightSide/BlockedUsers/BlockedUserMenu.tsx
+++ src/components/Chatroom/ChatroomRightSide/BlockedUsers/BlockedUserMenu.tsx
@@ -1,10 +1,36 @@
 import { FC } from "react"
 import "./blockedUsers.css"
+import customFetch from "../../../../utils"
+import { useAppDispatch, useAppSelector } from "../../../../store/storeHooks"
+import { updateUserField } from "../../../../features/userSlice"
 
 const BlockedUserMenu: FC<{ email: string }> = ({ email }) => {
+	const dispatch = useAppDispatch()
+
+	const { user } = useAppSelector((store) => store.user)
+
+	const unblockUser = async (
+		e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+	) => {
+		e.preventDefault()
+		// send request to unblock user
+		const { data } = await customFetch.post("/unblock-user", {
+			userId: user.id,
+			friendEmail: email,
+		})
+
+		// update fields in localstorage
+		const { blockedUsers, friends, messages } = data
+		dispatch(
+			updateUserField({ field: "blockedUsers", value: blockedUsers })
+		)
+		dispatch(updateUserField({ field: "friends", value: friends }))
+		dispatch(updateUserField({ field: "messages", value: messages }))
+	}
+
 	return (
 		<div className="blocked-menu">
-			<button type="button">
+			<button type="button" onClick={unblockUser}>
 				<p>Unblock</p>
 			</button>
 		</div>
