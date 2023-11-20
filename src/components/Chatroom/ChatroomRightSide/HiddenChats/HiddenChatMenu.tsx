c3bc6e0d3f12d8d68a12f3821069d37c73e96084
Janis <janislacis06@gmail.com>
2023-11-20 12:47:21 +0200

    Make UNHIDE CHATS function

--- src/components/Chatroom/ChatroomRightSide/HiddenChats/HiddenChatMenu.tsx
+++ src/components/Chatroom/ChatroomRightSide/HiddenChats/HiddenChatMenu.tsx
@@ -1,10 +1,32 @@
 import { FC } from "react"
 import "./hiddenChats.css"
+import { useAppDispatch, useAppSelector } from "../../../../store/storeHooks"
+import customFetch from "../../../../utils"
+import { updateUserField } from "../../../../features/userSlice"
 
 const HiddenChatMenu: FC<{ email: string }> = ({ email }) => {
+	const dispatch = useAppDispatch()
+	const { user } = useAppSelector((store) => store.user)
+
+	const unhideChats = async (
+		e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+	) => {
+		e.preventDefault()
+		const { data } = await customFetch.patch("/unhide-chat", {
+			email: email,
+			userId: user.id,
+		})
+		dispatch(
+			updateUserField({
+				field: "hiddenMessages",
+				value: data.hiddenMessages,
+			})
+		)
+	}
+
 	return (
 		<div className="hidden-menu">
-			<button type="button">
+			<button type="button" onClick={unhideChats}>
 				<p>Show (unhide) chats</p>
 			</button>
 		</div>
